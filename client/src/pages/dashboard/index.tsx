964beb10eaec1a531acf09185e58f2a97a2523cc
mayinuzzaman-shawon <mayinuzzaman@gmail.com>
2025-05-04 21:39:50 -0400

    updated file

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,4 +1,6 @@
 import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./financial-record-form";
+import { FinancialRecordList } from "./financial-record-list";
 
 export const Dashboard = () => {
 
@@ -7,6 +9,8 @@ export const Dashboard = () => {
     return (
     <div className="dashboard-container">
         <h1>Welcome {user?.firstName}, here is your finances:</h1>
+        <FinancialRecordForm/>
+        <FinancialRecordList/>
     </div>
     );
 }
