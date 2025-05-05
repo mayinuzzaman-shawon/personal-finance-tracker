db651933de7f353d82c1a79e6a810be279220bfe
mayinuzzaman-shawon <mayinuzzaman@gmail.com>
2025-05-05 06:43:27 -0400

    initial commit

--- client/src/pages/dashboard/financial-record-form.tsx
+++ client/src/pages/dashboard/financial-record-form.tsx
@@ -1,5 +1,58 @@
 export const FinancialRecordForm = () =>{
     return (
-         <div>Record Form</div>
+        <div className="form-container">
+        
+          <div className="form-field">
+            <label>Description:</label>
+            <input
+              type="text"
+              required
+              className="input"
+             
+            />
+          </div>
+          <div className="form-field">
+            <label>Amount:</label>
+            <input
+              type="number"
+              required
+              className="input"
+              
+            />
+          </div>
+          <div className="form-field">
+            <label>Category:</label>
+            <select
+              required
+              className="input"
+             
+            >
+              <option value="">Select a Category</option>
+              <option value="Food">Food</option>
+              <option value="Rent">Rent</option>
+              <option value="Salary">Salary</option>
+              <option value="Utilities">Utilities</option>
+              <option value="Entertainment">Entertainment</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+          <div className="form-field">
+            <label>Payment Method:</label>
+            <select
+              required
+              className="input"
+              
+            >
+              <option value="">Select a Payment Method</option>
+              <option value="Credit Card">Credit Card</option>
+              <option value="Cash">Cash</option>
+              <option value="Bank Transfer">Bank Transfer</option>
+            </select>
+          </div>
+          <button type="submit" className="button">
+            Add Record
+          </button>
+        
+      </div>
     );
 }
